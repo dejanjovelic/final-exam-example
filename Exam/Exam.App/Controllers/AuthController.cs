5c7c1344ce7c71201b25e745600c81aca7a02c23
Nikola Luburić <luburic91@gmail.com>
2025-11-07 09:42:56 +0100

    refactor: Simplifies project structure to avoid identity user special case.

---
+++ Exam/Exam.App/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using BuildingExample.Services;
+using BuildingExample.Services.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegistrationDTO data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _authService.Register(data);
+            return NoContent();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _authService.Login(data);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("profile")]
+        public async Task<IActionResult> Profile()
+        {
+            return Ok(await _authService.GetProfile(User));
+        }
+    }
+}
